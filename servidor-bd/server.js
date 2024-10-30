61e87a489670a67ba03faf6dfbda2529d702bcac
Filipe Rodrigues <filiperodrigueshomework@gmail.com>
2024-10-29 23:09:58 -0300

    criação do formulário de envio de pedido com registro no BD

--- servidor-bd/server.js
+++ servidor-bd/server.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   host: 'localhost',    // Host do PostgreSQL
   database: 'catalogo_digital', // Nome do banco de dados
   password: 'admin', // Senha do usuário
-  port: 5432,           // Porta padrão do PostgreSQL
+  port: 5435,           // Porta padrão do PostgreSQL
 });
 
 // Teste de conexão com o banco de dados
@@ -22,21 +22,41 @@ pool.connect((err, client, release) => {
 const app = express();
 app.use(express.json());
 
-// Rota para inserir um novo cliente
+// Rota para inserir Cliente
 app.post('/clientes', async (req, res) => {
-    const { nome, endereco, cpf, telefone } = req.body;
-  
-    try {
-      const result = await pool.query(
-        'INSERT INTO clientes (nome, endereco, cpf, telefone) VALUES ($1, $2, $3, $4) RETURNING *',
-        [nome, endereco, cpf, telefone]
-      );
-      res.status(201).json(result.rows[0]);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Erro ao inserir cliente');
-    }
-  });
+  const { nome, endereco, cpf, telefone, email } = req.body; // Incluindo o campo email
+
+  try {
+    const result = await pool.query(
+      'INSERT INTO clientes (nome, endereco, cpf, telefone, email) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+      [nome, endereco, cpf, telefone, email]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao inserir cliente');
+  }
+});
+
+// Rota para inserir Produto
+app.post('/produtos', async (req, res) => {
+  const { id, nome, descricao, preco, categoria } = req.body;
+
+  try {
+    const query = `
+      INSERT INTO produtos (nome, descricao, preco)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+    `;
+    const values = [nome, descricao, preco, categoria];
+
+    const result = await pool.query(query, values);
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao inserir produto' });
+  }
+});
 
 // Iniciar o servidor
 app.listen(3000, () => {
